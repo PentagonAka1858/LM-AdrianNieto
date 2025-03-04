2b2bb9fc0037650d40cc3e0da4ba94cc435dec0c
Adrián Nieto <adrian.niezam@protonmail.com>
2025-03-04 14:54:47 +0100

    Jquery

---
+++ tema4_javascript/ejerciciosParaPracticar/ejemplo.js
@@ -0,0 +1,28 @@
+/* Esperaremos hasta que el documento esté cargado y listo 
+ * para ser procesado por nuestro programa  */
+
+let obj_documento = $(document)
+
+/* Cuando esté cargado ejecutaremos la función cuyo nombre aparezca aquí */
+
+obj_documento.ready(inicio)
+
+//* Error gravisimo */
+//obj_documento.ready( inicio() )
+
+function inicio(){
+    let obj_izq=$("#botonizq")
+    obj_izq.click(fn_click_izq)
+    let obj_der=$("#botonder")
+    obj_der.click(fn_click_der)
+}
+
+function fn_click_izq(){
+    let obj_div=$(".texto")
+    obj_div.fadeOut()
+}
+
+function fn_click_der(){
+    let obj_div=$(".texto")
+    obj_div.fadeIn()
+}
